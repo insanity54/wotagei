a71ec3fe68e85aefc71689a1743820a05e66bd86
Chris Grimmett <chris@grimtech.net>
2019-03-21 20:18:24 -0700

    add upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
 
-sudo mount.davfs https://neocities.org/webdav ~/nww -o rw,uid=lbs
-rsync -vvzh --progress --delete --bwlimit=100 ./dist/ ~/nww/
+#!/bin/bash
+# Requires a ~/.netrc file with neocities.org credentials
+
+cd ./dist
+find . -type f -exec curl --progress-bar --verbose -o ~/Desktop/upload.log -n -T {} --create-dirs https://neocities.org/webdav/{} \;
+
+if [ ! $? -eq 0 ]; then
+  echo 'there was a problem while uploading files.'
+  exit 155
+else
+  echo 'all done! no errors.'
+  exit 0
+fi
